6e49044ad900ea496a993293bb6791531bf4d03d
Josip Paulik <josip.paulik@outlook.com>
2017-08-31 14:39:11 +0200

    Uncommented CauseRepository and inteface binding.

--- TrafficReporter.Repository/CauseRepository.cs
+++ TrafficReporter.Repository/CauseRepository.cs
@@ -1,58 +1,58 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Configuration;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Npgsql;
-//using TrafficReporter.Common;
-//using TrafficReporter.Common.Enums;
-//using TrafficReporter.Model;
-//using TrafficReporter.Model.Common;
-//using TrafficReporter.Repository.Common;
-//using TrafficReporter.Service.Common;
-//using Cause = TrafficReporter.Model.Cause;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using TrafficReporter.Common;
+using TrafficReporter.Common.Enums;
+using TrafficReporter.Model;
+using TrafficReporter.Model.Common;
+using TrafficReporter.Repository.Common;
+using TrafficReporter.Service.Common;
+using Cause = TrafficReporter.Model.Cause;
 
-//namespace TrafficReporter.Repository
-//{
-//    class CauseRepository : ICauseRepository
-//    {
-        
-//        public CauseRepository()
-//        {
-            
-//        }
+namespace TrafficReporter.Repository
+{
+    class CauseRepository : ICauseRepository
+    {
 
-//        public async Task<IEnumerable<ICause>> GetCausesAsync()
-//        {
-//            List<ICause> causes = new List<ICause>();
+        public CauseRepository()
+        {
 
-//            using (var connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["RemoteDB"]
-//                .ConnectionString))
-//            {
-//                await connection.OpenAsync();
+        }
 
-//                using (var command = new NpgsqlCommand("SELECT id, cause_str, icon_uri FROM cause_table ORDER BY id ASC", connection))
-//                {
-                   
-//                    using (var reader = await command.ExecuteReaderAsync())
-//                    {
-//                        while (reader.Read())
-//                        {
-//                            var cause = new Cause();
-//                            cause.Id = (int) reader.GetDataSafely("id", TODO);
-//                            cause.IconUri = reader.GetDataSafely("icon_uri", TODO).ToString();
-//                            cause.Name = reader.GetDataSafely("cause_str", TODO).ToString();
-//                            causes.Add(cause);
-//                        }
+        public async Task<IEnumerable<ICause>> GetCausesAsync()
+        {
+            List<ICause> causes = new List<ICause>();
 
-//                        reader.Close();
-//                    }
-//                }
-//                connection.Close();
-//            }
+            using (var connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["RemoteDB"]
+                .ConnectionString))
+            {
+                await connection.OpenAsync();
 
-//            return causes;
-//        }
-//    }
-//}
+                using (var command = new NpgsqlCommand("SELECT id, cause_str, icon_uri FROM cause_table ORDER BY id ASC", connection))
+                {
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                        {
+                            var cause = new Cause();
+                            cause.Id = (int)reader.GetDataSafely<int>("id");
+                            //cause.IconUri = reader.GetDataSafely("icon_uri", TODO).ToString();
+                            cause.Name = reader.GetDataSafely<string>("cause_str");
+                            causes.Add(cause);
+                        }
+
+                        reader.Close();
+                    }
+                }
+                connection.Close();
+            }
+
+            return causes;
+        }
+    }
+}
--- TrafficReporter.Repository/DIModule.cs
+++ TrafficReporter.Repository/DIModule.cs
@@ -11,7 +11,7 @@ namespace TrafficReporter.Repository
             //If someone wants to change how to get connection string, you can do it here.
             Bind<IReportRepository>().To<ReportRepository>().InSingletonScope()
                 .WithConstructorArgument(typeof(string), ConfigurationManager.ConnectionStrings["RemoteDB"].ConnectionString);
-            //Bind<ICauseRepository>().To<CauseRepository>();
+            Bind<ICauseRepository>().To<CauseRepository>();
         }
     }
 }
